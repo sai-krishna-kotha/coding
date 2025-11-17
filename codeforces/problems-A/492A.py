430c3fabd0664614e077b3aad155ed58cae25975
sai_krishna_kotha <kotha.sai.krishna.in@gmail.com>
2025-11-17 23:56:57 +0530

    striver cp sheet

--- codeforces/problems-A/492A.py
+++ codeforces/problems-A/492A.py
@@ -1,14 +1,14 @@
-# incomplete
-x = int(input())
-n = 0
+n = int(input())
+def sum_n(n: int):
+    return n*(n+1)/2
+ans = 0
 temp = 0
-i = 1
-if x == 1:
-    print(1)
-    exit()
-while n+temp+i < x:
-    temp = temp + i
-    n = n + temp
-    # print(f"{i, temp, n}")
-    i += 1
-print(i-1)
+while temp < n:
+    ans += 1
+    temp += sum_n(ans)
+    if temp > n:
+        print(ans-1)
+        break
+    if temp == n:
+        print(ans)
+
