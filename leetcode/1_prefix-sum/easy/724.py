4c31d98719c27ea18a5131e87cc9d6eb2757f358
sai_krishna_kotha <kotha.sai.krishna.in@gmail.com>
2025-11-19 01:33:30 +0530

    leetcode 724

--- leetcode/1_prefix-sum/easy/724.py
+++ leetcode/1_prefix-sum/easy/724.py
@@ -3,12 +3,9 @@ class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         right = sum(nums)
         left = 0
-        flag = True
         for i in range(len(nums)):
             right -= nums[i]
             if left == right:
-                flag = False
                 return i
             left += nums[i]
-        if flag:
-            return -1
+        return -1
